926fa8dcd39ff8580f4cd0cfe615686d2b6f5024
Karthik Reddy Maru <karthikreddy.maru@gmail.com>
2025-09-17 18:23:12 +0530

    add dns resolver for netty

---
+++ src/main/java/com/cashigo/gateway/config/RouteConfig.java
@@ -0,0 +1,25 @@
+package com.cashigo.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+
+    @Bean
+    RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route("user",path ->
+                        path
+                                .path("/user/**")
+                                .filters(filter ->
+                                   filter.rewritePath("", ""))
+                                .uri("http://localhost:8081")
+                )
+                .build();
+    }
+
+}
